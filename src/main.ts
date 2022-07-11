cc7216ec170ca10a9be26bda771a056befd1fd8c
sleek <tanbo_800@126.com>
2022-07-11 17:24:36 +0800

    ==

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-// var app = createApp(App);
-// app.component('vue-treeselect',VueTreeSelect);
-// app.mount('#app')
-
-createApp(App) 
-    .mount('#app');
